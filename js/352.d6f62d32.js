117fa3c1916fe6b6c6c2fed42815849d846aa2ac
Seindi Rahmat Barus <seindi@outlook.com>
2023-02-22 06:07:46 +0700

    .

---
+++ js/352.d6f62d32.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunk"]=self["webpackChunk"]||[]).push([[352],{352:(r,e,t)=>{t.r(e),t.d(e,{default:()=>c});var u=t(252);function n(r,e,t,n,o,a){(0,u.up)("vue");return(0,u.kq)("",!0)}var o=t(808);const a=o.ZP.router({id:"*",setup:function(){return o.ZP["export"]()},method:{layout(){if(o.ZP.app.router)return o.ZP.app.router.layout}}});var p=t(744);const s=(0,p.Z)(a,[["render",n]]),c=s}}]);
+//# sourceMappingURL=352.d6f62d32.js.map
